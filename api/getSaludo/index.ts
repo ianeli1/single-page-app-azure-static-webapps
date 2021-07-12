1d5ea404c2d8fe6362904fbea7fe125c51529509
ian <ianelicha@gmail.com>
2021-07-12 22:22:01 +0000

    listo

--- api/getSaludo/index.ts
+++ api/getSaludo/index.ts
@@ -1,13 +1,14 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import fetch from "node-fetch"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const responseMessage = Math.random()*100
+    const response = await fetch('https://rick-and-morty-api-phrases.herokuapp.com/phrases/en_us/random')
+    const json = await response.json()
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+     context.res.json({
+        text: json.phrase
+    });
 
 };
 
